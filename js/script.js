8e432c5de0883261b9a3e58ba78278d4e821a46e
IvanRecio <irecio@m-shop.mobi>
2024-11-12 19:23:44 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 import peliculas from './peliculas.js'
 
+//Los generos serán Acción (28), thriller(53), Aventura(12).
+
+var containerAcc28 = document.getElementById("genero-28")
+var containerThrll53 = document.getElementById("genero-53")
+var containerAdvtr12 = document.getElementById("genero-12")
+
+document.body.addEventListener('load', loadFilms())
+
+function loadFilms(){
+    debugger
+    for(var pelicula of peliculas){
+        var peliculaHTML = "<div class='pelicula'><img class='img-pelicula' src='https://image.tmdb.org/t/p/w500" + pelicula.poster_path + "' alt='img/Pelicula'><h3>" + pelicula.original_title + "</h3></div>"
+        //Peliculas de Accion
+        if(pelicula.genre_ids.includes(28)){
+            containerAcc28.innerHTML += peliculaHTML
+        }
+        //Peliculas de Thriller
+        if(pelicula.genre_ids.includes(53)){
+            containerThrll53.innerHTML += peliculaHTML
+        }
+        
+        //Peliculas de Aventura
+        if(pelicula.genre_ids.includes(12)){
+            containerAdvtr12.innerHTML += peliculaHTML
+        }
+    }
+}
